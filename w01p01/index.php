1378b0623ba56b0a31c5c2044165d79ece21834e
BartoszewskiA <bartoszewski.a@outlook.com>
2023-10-19 14:35:54 +0200

    first commit

---
+++ w01p01/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Test</title>
+</head>
+
+<body>
+  <?php
+  echo "<h1>Strona PHP</h1>";
+  echo '<p>Witaj Świecie</p>';
+  print("<p>To jest pierwszy program");
+  ?>
+</body>
+
+</html>
---
+++ w02p02/wynik.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title></title>
+  </head>
+  <body>
+    <h1>WYNIK</h1>
+    <?php
+    $x1 = $_GET["dana_1"];
+    $x2 = $_GET["dana_2"];
+    echo "<p>wynik= $x1+$x2</p>";
+    ?>
+  </body>
+</html>
